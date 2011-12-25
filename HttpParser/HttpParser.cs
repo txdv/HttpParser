0eae9e3622a775a783f56446df53ea8e0e6333a1
Andrius Bentkus <andrius.bentkus@gmail.com>
2011-12-25 18:50:30 +0200

    Initial commit.

---
+++ HttpParser/HttpParser.cs
@@ -0,0 +1,431 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Test
+{
+	[StructLayout(LayoutKind.Sequential)]
+	struct http_parser
+	{
+		byte typeFlags;
+		byte state;
+		byte header_state;
+		byte index;
+
+		uint nread;
+		ulong content_length;
+
+		// read only
+		public readonly short http_major;
+		public readonly short http_minor;
+		public readonly short status_code;
+		public readonly byte method;
+		public readonly byte errorUpgrade;
+
+		public IntPtr data;
+
+		public bool Upgrade {
+			get {
+				Console.WriteLine (http_minor);
+				return (errorUpgrade & 128) == 128;
+			}
+		}
+
+		public http_errno Error {
+			get {
+				return (http_errno)(errorUpgrade & 127);
+			}
+		}
+	}
+
+	/*
+	struct http_parser_settings_clr
+	{
+		public Func<IntPtr, int>            on_message_begin;
+		public Func<IntPtr, IntPtr, IntPtr> on_url;
+		public Func<IntPtr, IntPtr, IntPtr> on_header_field;
+		public Func<IntPtr, IntPtr, IntPtr> on_header_value;
+		public Func<IntPtr, int>            on_header_complete;
+		public Func<IntPtr, IntPtr, IntPtr> on_body;
+		public Func<IntPtr>                 on_message_complete;
+	}
+	*/
+
+	[StructLayout(LayoutKind.Sequential)]
+	struct http_parser_settings
+	{
+		public IntPtr on_message_begin;
+		public IntPtr on_url;
+		public IntPtr on_header_field;
+		public IntPtr on_header_value;
+		public IntPtr on_headers_complete;
+		public IntPtr on_body;
+		public IntPtr on_message_complete;
+	}
+
+	enum http_method
+	{
+		HTTP_DELETE,
+		HTTP_GET,
+		HTTP_HEAD,
+		HTTP_POST,
+		HTTP_PUT,
+		HTTP_CONNECT,
+		HTTP_OPTIONS,
+		HTTP_TRACE,
+		HTTP_COPY,
+		HTTP_LOCK,
+		HTTP_MKCOL,
+		HTTP_MOVE,
+		HTTP_PROPFIND,
+		HTTP_PROPATCH,
+		HTTP_UNLOCK,
+		HTTP_REPORT,
+		HTTP_MKACTIVITY,
+		HTTP_CHECKOUT,
+		HTTP_MERGE,
+		HTTP_MSEARCH,
+		HTTP_NOTIFY,
+		HTTP_SUSCRIBE,
+		HTTP_UNSUBSCRIBE,
+		HTTP_PATCH,
+	}
+
+	public enum http_parser_type
+	{
+		HTTP_REQUEST,
+		HTTP_RESPONSE,
+		HTTP_BOTH
+	}
+
+	enum http_errno
+	{
+		HPE_OK
+	}
+
+	class ByteBuffer : IDisposable
+	{
+		public byte[] Data { get; protected set; }
+		public GCHandle GCHandle { get; protected set; }
+		public IntPtr Pointer {
+			get {
+				return GCHandle.AddrOfPinnedObject();
+			}
+		}
+		public int Start {
+			get {
+				return (int)Pointer;	
+			}
+		}
+		public int Position(IntPtr at)
+		{
+			return (int)at - Start;
+		}
+
+		public ByteBuffer(byte[] data)
+		{
+			Data = data;
+			GCHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+		}
+
+		public void Dispose()
+		{
+			GCHandle.Free();
+		}
+	}
+
+	unsafe public class HttpParser : IDisposable
+	{
+		GCHandle gchandle;
+		http_parser data;
+		http_parser_settings settings;
+
+		Func<IntPtr, int>                 onMessageBegin;
+		Func<IntPtr, IntPtr, IntPtr, int> onUrl;
+		Func<IntPtr, IntPtr, IntPtr, int> onHeaderField;
+		Func<IntPtr, IntPtr, IntPtr, int> onHeaderValue;
+		Func<IntPtr, int>                 onHeadersComplete;
+		Func<IntPtr, IntPtr, IntPtr, int> onBody;
+		Func<IntPtr, int>                 onMessageComplete;
+
+		ByteBuffer buffer;
+
+		public HttpParser()
+			: this(http_parser_type.HTTP_BOTH)
+		{
+		}
+
+		public HttpParser(http_parser_type type)
+		{
+			data = new http_parser();
+			GCHandle.Alloc(this, GCHandleType.Pinned);
+			http_parser_init(DataPointer, type);
+			
+			onMessageBegin    = OnMessageBegin;
+			onUrl             = OnUrl;
+			onHeaderField     = OnHeaderField;
+			onHeaderValue     = OnHeaderValue;
+			onHeadersComplete = OnHeadersComplete;
+			onBody            = OnBody;
+			onMessageComplete = OnMessageComplete;
+
+			settings.on_message_begin    = Marshal.GetFunctionPointerForDelegate(onMessageBegin);
+			settings.on_url              = Marshal.GetFunctionPointerForDelegate(onUrl);
+			settings.on_header_field     = Marshal.GetFunctionPointerForDelegate(onHeaderField);
+			settings.on_header_value     = Marshal.GetFunctionPointerForDelegate(onHeaderValue);
+			settings.on_headers_complete = Marshal.GetFunctionPointerForDelegate(onHeadersComplete);
+			settings.on_body             = Marshal.GetFunctionPointerForDelegate(onBody);
+			settings.on_message_complete = Marshal.GetFunctionPointerForDelegate(onMessageComplete);
+		}
+
+		int OnMessageBegin(IntPtr ptr)
+		{
+			return OnMessageBegin();
+		}
+		int OnUrl(IntPtr ptr, IntPtr at, IntPtr length)
+		{
+			return OnUrl(buffer.Data, buffer.Position(at), (int)length);
+		}
+		int OnHeaderField(IntPtr ptr, IntPtr at, IntPtr length)
+		{
+			return OnHeaderField(buffer.Data, buffer.Position(at), (int)length);
+		}
+		int OnHeaderValue(IntPtr ptr, IntPtr at, IntPtr length)
+		{
+			return OnHeaderValue(buffer.Data, buffer.Position(at), (int)length);
+		}
+		int OnHeadersComplete(IntPtr ptr)
+		{
+			return OnHeadersComplete();
+		}
+		int OnBody(IntPtr ptr, IntPtr at, IntPtr length)
+		{
+			return OnBody(buffer.Data, buffer.Position(at), (int)length);
+		}
+		int OnMessageComplete(IntPtr ptr)
+		{
+			return OnMessageComplete();
+		}
+
+		public virtual int OnMessageBegin()
+		{
+			return 0;
+		}
+		public virtual int OnUrl(byte[] data, int start, int count)
+		{
+			return 0;
+		}
+		public virtual int OnHeaderField(byte[] data, int start, int count)
+		{
+			return 0;
+		}
+		public virtual int OnHeaderValue(byte[] data, int start, int count)
+		{
+			return 0;
+		}
+		public virtual int OnHeadersComplete()
+		{
+			return 0;
+		}
+		public virtual int OnBody(byte[] data, int start, int count)
+		{
+			return 0;
+		}
+		public virtual int OnMessageComplete()
+		{
+			return 0;
+		}
+
+		IntPtr DataPointer {
+			get {
+				fixed (http_parser *dataPtr = &data) {
+					return (IntPtr)dataPtr;	
+				}
+			}
+		}
+
+		IntPtr SettingsPointer {
+			get {
+				fixed (http_parser_settings *settingsPtr = &settings) {
+					return (IntPtr)settingsPtr;
+				}
+			}
+		}
+
+		public bool Upgrade {
+			get {	
+				return data.Upgrade;
+			}
+		}
+
+		public void Execute(byte[] data, int length)
+		{
+			using (buffer = new ByteBuffer(data))
+			{
+				http_parser_execute(DataPointer, SettingsPointer, buffer.Pointer, (IntPtr)length);
+			}
+			buffer = null;
+		}
+
+		public void Execute(byte[] data)
+		{
+			Execute(data, data.Length);
+		}
+
+		public void Execute(Encoding enc, string str)
+		{
+			byte[] data = enc.GetBytes(str);
+			Execute(data);
+		}
+
+		public void Dispose()
+		{
+			gchandle.Free();
+		}
+
+		[DllImport("http_parser")]
+		private static extern void http_parser_init(IntPtr parser, http_parser_type type);
+
+		[DllImport("http_parser")]
+		private static extern IntPtr http_parser_execute(IntPtr parser, IntPtr settings, IntPtr data, IntPtr length);
+
+		[DllImport("http_parser")]
+		private static extern int http_should_keep_alive(IntPtr parser);
+
+		[DllImport("http_parser")]
+		private static extern sbyte *http_method_str(http_method m);
+
+		[DllImport("http_parser")]
+		private static extern sbyte *http_errno_name(http_errno err);
+
+		[DllImport("http_parser")]
+		private static extern sbyte *http_errno_description(http_errno err);
+	}
+
+	public class EventedHttpParser : HttpParser
+	{
+		public EventedHttpParser()
+			: base()
+		{
+		}
+
+		public EventedHttpParser(http_parser_type type)
+			: base(type)
+		{
+		}
+
+		public event Action OnMessageBeginEvent;
+		public override int OnMessageBegin()
+		{
+			if (OnMessageBeginEvent != null) {
+				OnMessageBeginEvent();	
+			}
+			
+			return base.OnMessageBegin();
+		}
+
+		public event Action<byte[], int, int> OnUrlEvent;
+		public override int OnUrl(byte[] data, int start, int count)
+		{
+			if (OnUrlEvent != null) {
+				OnUrlEvent(data, start, count);
+			}
+			return base.OnUrl (data, start, count);
+		}
+
+		public event Action<byte[], int, int> OnHeaderFieldEvent;
+		public override int OnHeaderField(byte[] data, int start, int count)
+		{
+			if (OnHeaderFieldEvent != null) {
+				OnHeaderFieldEvent(data, start, count);
+			}
+
+			return base.OnHeaderField(data, start, count);
+		}
+
+		public event Action<byte[], int, int> OnHeaderValueEvent;
+		public override int OnHeaderValue(byte[] data, int start, int count)
+		{
+			if (OnHeaderValueEvent != null) {
+				OnHeaderValueEvent(data, start, count);
+			}
+
+			return base.OnHeaderValue(data, start, count);
+		}
+
+		public event Action OnHeadersCompleteEvent;
+		public override int OnHeadersComplete()
+		{
+			if (OnHeadersCompleteEvent != null) {
+				OnHeadersCompleteEvent();
+			}
+
+			return base.OnHeadersComplete ();
+		}
+
+		public event Action<byte[], int, int> OnBodyEvent;
+		public override int OnBody(byte[] data, int start, int count)
+		{
+			if (OnBodyEvent != null) {
+				OnBodyEvent(data, start, count);
+			}
+			return base.OnBody(data, start, count);
+		}
+
+		public event Action OnMessageCompleteEvent;
+		public override int OnMessageComplete()
+		{
+			if (OnMessageCompleteEvent != null) {
+				OnMessageCompleteEvent();
+			}
+
+			return base.OnMessageComplete();
+		}
+	}
+
+	public class EncodedHttpParser : EventedHttpParser
+	{
+		public Encoding Encoding { get; protected set; }
+
+		public EncodedHttpParser()
+			: this(Encoding.Default)
+		{
+		}
+
+		public EncodedHttpParser(Encoding enc)
+			: this(http_parser_type.HTTP_BOTH, enc)
+		{
+		}
+
+		public EncodedHttpParser(http_parser_type type)
+			: this(type, Encoding.Default)
+		{
+		}
+
+		public EncodedHttpParser(http_parser_type type, Encoding enc)
+			: base(type)
+		{
+			Encoding = enc;
+		}
+
+		public Action<string, string> OnHeaderElementEvent;
+
+		string field = null;
+		public override int OnHeaderField(byte[] data, int start, int count)
+		{
+			field = Encoding.GetString(data, start, count);
+
+			return base.OnHeaderField(data, start, count);
+		}
+
+		public override int OnHeaderValue(byte[] data, int start, int count)
+		{
+			if (OnHeaderElementEvent != null) {
+				OnHeaderElementEvent(field, Encoding.GetString(data, start, count));
+			}
+
+			return base.OnHeaderValue (data, start, count);
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+HttpParser
+==========
+
+This is a C# wrapper for the awesome [http_parser library](https://github.com/joyent/http-parser).
+
+This wrapper requires a shared library version of httpparser.
+You can my modified version of [http_parser](https://github.com/txdv/http-parser) where you just have to
+type make libhttp_parser.so in order to create the shared version.
+
+License
+=======
+The wrapper is available under the MIT license.
